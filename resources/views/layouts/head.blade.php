65dc4793e690bc38265e72ff6cb4e619e708f1c1
Priyanshi Sachan <87604551+priyanshiisingh@users.noreply.github.com>
2025-04-22 01:44:50 +0530

    updat title and favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,9 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="author" content="ThemeMarch">
 	<!-- Page Title -->
-	<title>Helixial Laboratories</title>
+	<title>Helixial</title>
     <!-- Favicon Icon -->
-  	<link rel="icon" href="assets/img/favicon.png">
+  	<link rel="icon" href="assets/img/client/logo/helixial_logo.png">
 	<!-- Stylesheets -->
     <link rel="stylesheet" type="text/css" href="assets/css/plugins.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
