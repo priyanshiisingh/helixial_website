fa404db578f80d78d2d77f5b799842d3bf58b577
Priyanshi Sachan <87604551+priyanshiisingh@users.noreply.github.com>
2025-05-23 04:56:05 +0530

    cancer pages

--- resources/views/modules/services/cancerFind.blade.php
+++ resources/views/modules/services/cancerFind.blade.php
@@ -18,50 +18,48 @@
     </section> --}}
     <!-- End Blog Header -->
     <div class="empty-space col-md-b80 col-xs-b80"></div>
+    <div class="empty-space col-md-b80 col-xs-b80"></div>
+
+    <div class="tm-blog-section tm-blog-left-sidebar">
+        <div class="container">
+            <div class="about-content mb-0">
+                <div class="w-100" data-aos="fade-right" data-aos-duration="3000">
+                    <h1 class="section-title">Cancer<strong>Find</strong></h1>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Start Site Content -->
-    <div class="tm-blog-section tm-blog-left-sidebar tm-gradient-blog-bg">
+    <!-- Start Site Content -->
+    <div id="cancerFind" class="tm-blog-section tm-blog-left-sidebar">
         <div class="container">
-            <div class="row">
-                <main class="container mt-3">
-                    <article data-aos="zoom-in">
-                        <header class="mb-4" data-aos="fade-down">
-                            <h1 class="text-primary text-white">CancerFind<sup>™</sup></h1>
-                        </header>
-                        <div class="mb-5 text-white" data-aos="fade-up">
-                            <p>CancerFind™ is a revolutionary blood-based test for Multiple Cancer Early Detection
-                                (MCED)—designed to identify the presence of various cancer types through a single,
-                                non-invasive blood draw.
-                                <br>
-                                <br>
-                                It offers a convenient screening solution for asymptomatic individuals and those at high
-                                risk, making it easy to incorporate into routine medical checkups. Beyond detection,
-                                CancerFind™ provides accurate predictions of the tissue of origin of any cancer signal,
-                                enabling faster and more targeted clinical decision-making. It can prove to be crucial for
-                                individuals who want early confirmation of cancer, those with a family history, or those
-                                needing further examination after screenings.
-                                By facilitating early diagnosis and guiding next steps, CancerFind™ plays a critical role in
-                                improving patient outcomes and advancing proactive cancer care.
+            <div class="about-content mb-0 p-0">
+                <div class="about-content-new" data-aos="fade-left" data-aos-duration="3000">
+                    <p>
+                        CancerFind™ is a revolutionary blood-based test for <b>Multiple Cancer Early Detection
+                            (MCED)</b>—designed to identify the presence of various cancer types through a single,
+                        non-invasive
+                        blood draw.
+                        <br><br>
+                        It offers a convenient screening solution for asymptomatic individuals and those at high risk,
+                        making it easy to incorporate into routine medical checkups. Beyond detection, CancerFind™
+                        provides accurate predictions of the tissue of origin of any cancer signal, enabling faster and
+                        more targeted clinical decision-making. It can prove to be crucial for individuals who want
+                        early confirmation of cancer, those with a family history, or those needing further examination
+                        after screenings.
+                        <br><br>
+                        By facilitating early diagnosis and guiding next steps, CancerFind™ plays a criticalrole in
+                        improving patient outcomes and advancing proactive cancer care.
+                    </p>
+                </div>
+                <div class="w-100 mt-2" data-aos="fade-right" data-aos-duration="3000">
+                    <img src="{{ asset('assets/img/new/alpha/sampleTypes.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Site Content -->
 
-                            </p>
-                        </div>
-                    </article>
-                </main>
-            </div>
-        </div>
-    </div>
-    <div class="tm-blog-section tm-blog-left-sidebar tm-7098a9-bg">
-        <div class="container">
-            <div class="row">
-                <main class="container mt-3">
-                    <header class="mb-4" data-aos="fade-down">
-                        <h2 class="mt-2">The test procedure</h2>
-                    </header>
-                    <img src="{{ asset('assets/img/client/helixial_1.jpg') }}" alt="assets/img/client/helixial_1.jpg">
-                </main>
-            </div>
-        </div>
-        <div class="empty-space col-md-b80 col-xs-b80"></div>
-    </div>
     <!-- End Site Content -->
 @endsection
