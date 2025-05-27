7beab78da624960b4d8c7bf1b216449c5dddc78e
Priyanshi Sachan <87604551+priyanshiisingh@users.noreply.github.com>
2025-05-27 23:45:17 +0530

    seo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
             <p class="mb-2 mb-md-0 tm-copyright">Copyright © 2025 Helixial.</p>
             <div class="d-flex align-items-center">
                 <span class="me-2">Follow Us</span>
-                <a href="https://helixial-services-pvt-ltd.linkedin.com" target="_blank" class="text-white">
+                <a href="https://helixial-services-pvt-ltd.linkedin.com" target="_blank" class="text-white linkedin-text">
                     <i class="fa fa-linkedin-square fa-lg" style="background: #75E2DF; color: white; padding: 4px 6px; border-radius: 4px;"></i>
                 </a>
             </div>
